7be550689efa360eaab07c5c99d1215cb971922a
Bartosz <berthemoose@gmail.com>
2025-10-27 20:42:40 +0100

    testing w/o authentication

--- composables/useGetArchivedOrderList.ts
+++ composables/useGetArchivedOrderList.ts
@@ -1,24 +1,16 @@
 import type {Orders} from "../types/order";
 import { useApi } from "./useApi";
-import { useAuthenticatedFetch } from "./useAuthenticatedFetch";
 
 export const useGetArchivedOrderList = (limit: number = 50, skip: number = 0) => {
     const {orderUrl} = useApi();
-    const { authenticatedUseFetch } = useAuthenticatedFetch();
     
     const url = `${orderUrl}/archived-orders?limit=${limit}&skip=${skip}`;
     console.log('Fetching archived orders:', url);   
     
-    const {data, error, refresh} = authenticatedUseFetch<Orders>(url,
-        {
-            key: `archived-order-list-${skip}-${limit}`,
-            lazy: true,
-            server: false,
-            getCachedData(key) {
-                return undefined;
-            },
-        }
-    );
+    const {data, error, refresh} = useFetch<Orders>(url, {
+        lazy: true,
+        server: false,
+    });
     
     return {
         data,
